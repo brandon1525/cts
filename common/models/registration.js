6fdbd766b8f29c7d56a852366c7152e5c9252614
Brandon Alan Rodríguez Martínez <br4nd0n4l4n@gmail.com>
2017-10-02 16:45:23 -0500

    Add remote method and acl

--- common/models/registration.js
+++ common/models/registration.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function(Registration) {
-
+  Registration.schedule = function(msg, cb) {
+    cb(null, msg);
+  }
+  Registration.remoteMethod('schedule', {
+    accepts: { arg: 'data', type: 'object', http: { source: 'body' }, description: 'Instance of schedule' },
+    returns: [
+      { arg: 'student', type: 'string' },
+      { arg: 'course', type: 'string' },
+      { arg: 'semester', type: 'number' },
+      { arg: 'id', type: 'string' }
+    ],
+    description: 'Method is used to schedule a course'
+  });
 };
