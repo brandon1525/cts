65782a6e401921cea27e5bbefea8411078fec169
Brandon Alan Rodríguez Martínez <br4nd0n4l4n@gmail.com>
2017-10-03 20:42:47 -0500

    Operation hooks

--- common/models/course.js
+++ common/models/course.js
@@ -1,5 +1,29 @@
 'use strict';
 
 module.exports = function(Course) {
+  var app = require('../../server/server');
+  Course.observe('loaded', function(ctx, next) {
+    //console.log('ctx.data', ctx.data);
+    let Registration = app.models.Registration;
+    /*let course = {
+      name : ctx.data.name,
+      description: ctx.data.description
+    };*/
+    Registration.find(
+      {
+        where: { course: ctx.data.id },
+        include: [
+          {
+            relation: 'registers',
+            scope: {
+              fields: ['name', 'major', 'email', 'id']
+            }
+          }
+        ]
+      }, (err, courses)=>{
+      console.log(courses);
+    });
 
+    next();
+  });
 };
